82d02e264896eab9f33e97625e446c2ec24eb772
Tert <56269917+Tertenea@users.noreply.github.com>
2022-12-13 10:01:29 +0200

    Add files via upload

---
+++ kordamine 1.ps1
@@ -0,0 +1,23 @@
+﻿#P.Kiviorg
+#13.12.2022
+#Nimed emailist
+
+$skriptiAsukoht = $MyInvocation.MyCommand.Path
+$dir = Split-Path $skriptiAsukoht
+
+$fail = Read-Host -Prompt "Sisesta csv faili nimi"
+if($fail -like '*.csv*') {$import = Import-Csv $dir\$fail} 
+else {Write-Host "Antud fail pole csv fail"}
+foreach ($User in $import)
+{
+$email = $User.emails
+$nimiemailis = $email.split("@")[0]
+
+$enimi = $nimiemailis.Split(".")[0]
+$pnimi = $nimiemailis.Split(".")[1]
+
+$krenimi = $enimi.substring(0,1).toupper()+$enimi.substring(1).tolower()
+$krpnimi = $pnimi.substring(0,1).toupper()+$pnimi.substring(1).tolower()
+
+$krenimi+" "+$krpnimi >> $dir\nimed.txt
+}
